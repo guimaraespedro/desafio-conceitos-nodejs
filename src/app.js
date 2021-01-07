e2c3d4c4979302da3c739892b3847097ea35cad7
pedro guimaraes <pedrof.guima197@gmail.com>
2021-01-06 21:39:00 -0300

    resolução do desafio em nodeJS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,87 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const {title, url, techs} = request.body;
+
+  const project = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0
+  }
+
+  repositories.push(project);
+
+  return response.json(project);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const {id} = request.params;
+
+  const {title, url, techs} = request.body;
+
+  if(!isUuid(id)){
+    return response.status(400).send({message: "invalid Id"});
+  }
+
+  const projectIndex = repositories.findIndex(project=> project.id===id);
+
+  if(projectIndex<0){
+    return response.status(400).send({message:"id not found"});
+  }
+
+  repositories[projectIndex] = {...repositories[projectIndex], title, url, techs};
+  console.log(repositories[projectIndex]);
+
+  return response.json(repositories[projectIndex]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).send({message: "invalid Id"});
+  }
+
+  const projectIndex = repositories.findIndex(project=> project.id===id);
+
+  if(projectIndex<0){
+    return response.status(400).send({message:"id not found"});
+  }
+
+  repositories.slice(projectIndex, 1);
+  
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).send({message: "invalid Id"});
+  }
+
+  const projectIndex = repositories.findIndex(project=> project.id===id);
+
+  if(projectIndex<0){
+    return response.status(400).send({message:"id not found"});
+  }
+
+  repositories[projectIndex].likes++;
+
+  return response.json(repositories[projectIndex]);
+
 });
 
 module.exports = app;
